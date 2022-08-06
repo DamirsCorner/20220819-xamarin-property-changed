bacfcd2275d09a1ee721ff7daf0cd1d0d7210d02
Damir Arh <damir.arh@gmail.com>
2022-08-06 12:58:34 +0200

    Data binding without base view model

---
+++ XamarinPropertyChanged/XamarinPropertyChanged/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace XamarinPropertyChanged
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private List<string> items = new List<string>();
+
+        public List<string> Items
+        {
+            get { return items; }
+            set
+            {
+                items = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            LoadData();
+        }
+
+        async void LoadData()
+        {
+            await Task.Delay(500);
+            Items = new List<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5"
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
